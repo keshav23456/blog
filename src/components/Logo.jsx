0fc3b848a6adaf52a90921c30b6a6910970ace8f
keshav23456 <keshav.ug23@nsut.ac.in>
2025-03-25 03:42:47 +0530

    in

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
-import react from "../assets/react.svg";  // Renaming for clarity
+// import React from 'react';
+// import react from "../assets/react.svg";  // Renaming for clarity
 
-function Logo({ width = "100px" }) {
+// function Logo({ width = "100px" }) {
+//   return (
+//     <div>
+//       <img src={react} alt="logo" style={{ width }} />
+//     </div>
+//   );
+// }
+
+// export default Logo;
+
+
+import React from 'react';
+import logoImage from "../assets/react.svg";
+
+function Logo({ 
+  width = "100px", 
+  height = "auto", 
+  className = "", 
+  onClick = null,
+  withText = false,
+  textClassName = ""
+}) {
   return (
-    <div>
-      <img src={react} alt="logo" style={{ width }} />
+    <div 
+      className={`flex items-center ${className}`}
+      onClick={onClick}
+      style={{ cursor: onClick ? 'pointer' : 'default' }}
+    >
+      <img 
+        src={logoImage} 
+        alt="Company Logo" 
+        style={{ width, height }}
+        className="object-contain"
+      />
+      {withText && (
+        <span className={`ml-2 font-bold ${textClassName}`}>
+          DevUI
+        </span>
+      )}
     </div>
   );
 }
